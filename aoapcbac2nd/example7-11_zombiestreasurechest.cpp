ae5f2dc97a74783a92aa3a4c1fb53c12e818560d
aoibird <aoi@aoibird.com>
2019-08-02 18:23:20 +0800

    Submit AOAPC BAC 2nd Example 7-11, Zombie's Treasure Chest, UVa 12325 (TLE)

--- aoapcbac2nd/example7-11_zombiestreasurechest.cpp
+++ aoapcbac2nd/example7-11_zombiestreasurechest.cpp
@@ -7,7 +7,7 @@
 #include <algorithm>
 using namespace std;
 
-int dp[(1 << 20)];
+//int dp[(1 << 20)];
 int N, S1, V1, S2, V2;
 
 int main()
@@ -18,18 +18,33 @@ int main()
     scanf("%d%d%d%d%d", &N, &S1, &V1, &S2, &V2);
 
     //memset(dp, 0, sizeof(dp)); // too slow
-    for (int i = 0; i <= N; i++) dp[i] = 0;
+    // for (int i = 0; i <= N; i++) dp[i] = 0;
+    // for (int i = 0; i < 2; i++) {
+    //   int s, v;
+    //   if (i == 0) { s = S1; v = V1; }
+    //   else { s = S2; v = V2; }
+    //   for (int j = s; j <= N; j++) {
+    //     dp[j] = max(dp[j-s] + v, dp[j]);
+    //   }
+    // }
 
-    for (int i = 0; i < 2; i++) {
-      int s, v;
-      if (i == 0) { s = S1; v = V1; }
-      else { s = S2; v = V2; }
-      for (int j = s; j <= N; j++) {
-        dp[j] = max(dp[j-s] + v, dp[j]);
+    int best = 0;
+    if (N/S1 < N/S2) {
+      for (int i = 0; i*S1 <= N; i++) {
+        int j = (N-i*S1) / S2;
+        int v = i*V1 + j*V2;
+        if (v > best) best = v;
+      }
+    }
+    else {
+      for (int i = 0; i*S2 <= N; i++) {
+        int j = (N-i*S2) / S1;
+        int v = i*V2 + j *V1;
+        if (v > best) best = v;
       }
     }
 
-    printf("Case #%d: %d\n", k+1, dp[N]);
+    printf("Case #%d: %d\n", k+1, best);
   }
 
 }
