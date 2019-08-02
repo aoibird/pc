f8f73afc6d1e1f9ae8f411b790724e5e1bbb6b7f
aoibird <aoi@aoibird.com>
2019-08-02 17:49:28 +0800

    Submit AOAPC BAC 2nd Example 7-11, Zombie's Treasure Chest, UVa 12325 (CE)

---
+++ aoapcbac2nd/example7-11_zombiestreasurechest.cpp
@@ -0,0 +1,35 @@
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[(1 << 30)];
+int N, S1, V1, S2, V2;
+
+int main()
+{
+  int T;
+  scanf("%d", &T);
+  for (int k = 0; k < T; k++) {
+    scanf("%d%d%d%d%d", &N, &S1, &V1, &S2, &V2);
+
+    //memset(dp, 0, sizeof(dp)); // too slow
+    for (int i = 0; i <= N; i++) dp[i] = 0;
+
+    for (int i = 0; i < 2; i++) {
+      int s, v;
+      if (i == 0) { s = S1; v = V1; }
+      else { s = S2; v = V2; }
+      for (int j = s; j <= N; j++) {
+        dp[j] = max(dp[j-s] + v, dp[j]);
+      }
+    }
+
+    printf("Case #%d: %d\n", k+1, dp[N]);
+  }
+
+}
