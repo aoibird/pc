a70df1f6c95ab52955ca3f6b1e657ec3539b6b67
aoibird <aoi@aoibird.com>
2019-10-01 22:26:20 +0800

    Submit CPC Exercise 2-21, Sumsets, POJ 2229 (WA)

---
+++ cpc/exercise2-21_sumsets.cpp
@@ -0,0 +1,29 @@
+// POJ 2229
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+const int MAXN = 1000000;
+ll DP[MAXN];
+int N;
+
+
+int main()
+{
+  scanf("%d", &N);
+  fill(DP, DP+N+1, 1);
+  for (int k = 1; (1 << k) <= N; k++) {
+    for (int s = 1<<k; s <= N; s++) {
+      DP[s] = DP[s] + DP[s-(1<<k)];
+      //printf("%lld ", DP[s]);
+    }
+    //printf("\n");
+  }
+  printf("%lld\n", DP[N] % 1000000000);
+}
