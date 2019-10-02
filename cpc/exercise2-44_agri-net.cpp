fd146a7bd0d748a14f7394eb736231424e13d5ed
aoibird <aoi@aoibird.com>
2019-10-02 16:26:09 +0800

    Submit CPC Exercise 2-44, Agri-Net, POJ 1258 (WA)

---
+++ cpc/exercise2-44_agri-net.cpp
@@ -0,0 +1,57 @@
+// POJ 1258
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+typedef pair<int,int> PII;
+const int INF = 10000000;
+const int MAXN = 100+10;
+int N; // [3, 100]
+vector<PII> G[MAXN];
+int D[MAXN];
+int S;
+
+void prim()
+{
+  fill(D, D+N, INF);
+  priority_queue<PII, vector<PII>, greater<PII> > pq;
+  D[0] = 0;
+  pq.push(PII(0, 0));
+  S = 0;
+  while (!pq.empty()) {
+    PII curr = pq.top(); pq.pop();
+    int c = curr.first;
+    int v = curr.second;
+    if (c > D[v]) continue;
+    S += c;
+    for (int u = 0; u < G[v].size(); u++) {
+      PII e = G[v][u];
+      if (D[e.first] > e.second) {
+        D[e.first] = e.second;
+        pq.push(PII(D[e.first], e.first));
+      }
+    }
+  }
+}
+
+int main()
+{
+  scanf("%d", &N);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      int c; scanf("%d", &c);
+      if (c != 0) G[i].push_back(PII(j, c));
+    }
+  }
+
+  prim();
+
+  printf("%d\n", S);
+}
