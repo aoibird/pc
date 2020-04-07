fc223527266a3ad6b4bd6b0ab922aa988dd9ff1d
aoibird <aoi@aoibird.com>
2020-04-07 22:00:35 +0800

    Submit Codeforces Problem 189 A, Cut Ribbon, Codeforces Problem 189 A (AC)

--- codeforces/problem189a_cutribbon.cpp
+++ codeforces/problem189a_cutribbon.cpp
@@ -8,16 +8,21 @@
 using namespace std;
 
 const int MAXN = 4000+10;
+const int K = 3;
 int DP[MAXN];
+int N;
+int C[K];
 
 int main()
 {
-  int N, a, b, c;
-  scanf("%d%d%d%d", &N, &a, &b, &c);
-  DP[a] = DP[b] = DP[c] = 1;
-  int s = max(a, max(b, c));
-  for (int i = s; i <= N; i++) {
-    DP[i] = max(DP[i-a], max(DP[i-b], DP[i-c])) + 1;
+  scanf("%d%d%d%d", &N, &C[0], &C[1], &C[2]);
+  memset(DP, 0, sizeof(DP));
+  for (int i = 0; i < K; i++) DP[C[i]] = 1;
+  for (int i = 0; i <= N; i++) {
+    for (int j = 0; j < K; j++) {
+      if (i - C[j] >= 0 && DP[i-C[j]] > 0)
+        DP[i] = max(DP[i], DP[i-C[j]] + 1);
+    }
   }
   // for (int i = 0; i <= N; i++) printf("%d%c", DP[i],i==N?'\n':' ');
   printf("%d\n", DP[N]);
