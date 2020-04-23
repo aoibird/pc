2b4314fec52479ce68375acdc020f92149487d00
aoibird <aoi@aoibird.com>
2020-04-23 11:51:04 +0800

    Submit POJ 1061, 青蛙的约会, POJ 1061 (WA)

---
+++ poj/1061_青蛙的约会.cpp
@@ -0,0 +1,33 @@
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+ll gcd(ll a, ll b)
+{
+  return (b == 0) ? a : gcd(b, a % b);
+}
+
+void exgcd(ll a, ll b, ll& d, ll& x, ll& y)
+{
+  if (b == 0) { d = a; x = 1; y = 0; }
+  else { exgcd(b, a % b, d, y, x); y -= (a / b) * x; }
+}
+
+int main()
+{
+  ll x, y, m, n, L;
+  scanf("%lld%lld%lld%lld%lld", &x, &y, &m, &n, &L);
+  ll a = ((m-n) + L) % L;
+  ll b = ((y-x) + L) % L;
+  ll d = 0, k = 0, g = 0;
+  ll thegcd = gcd(a, L);
+  // printf("%lld k - %lld g = %lld\n", a, L, b);
+  if (b % thegcd != 0) printf("Impossible\n");
+  else { exgcd(a, L, d, k, g); printf("%lld\n", (k*(thegcd/b) + L) % L); }
+}
