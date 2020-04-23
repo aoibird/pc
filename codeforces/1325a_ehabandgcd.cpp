f76a2f31898937aab40ce72af62ba244cb3ad315
aoibird <aoi@aoibird.com>
2020-04-23 15:39:13 +0800

    Submit Codeforces 1325 A, EhAb AnD gCd, Codeforces 1325 A (AC)

---
+++ codeforces/1325a_ehabandgcd.cpp
@@ -0,0 +1,17 @@
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int TC; scanf("%d", &TC);
+  for (int i = 0; i < TC; i++) {
+    int x; scanf("%d", &x);
+    printf("%d %d\n", 1, x-1);
+  }
+}
