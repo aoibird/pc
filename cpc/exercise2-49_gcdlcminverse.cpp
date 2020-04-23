ad89edbfc399763f559669d73f6bdc53ce815421
aoibird <aoi@aoibird.com>
2020-04-23 09:58:47 +0800

    Submit CPC Exercise 2-49, GCD & LCM Inverse, POJ 2429 (TLE)

--- cpc/exercise2-49_gcdlcminverse.cpp
+++ cpc/exercise2-49_gcdlcminverse.cpp
@@ -6,27 +6,27 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 typedef long long ll;
+
+vector<int> divisors;
+
 int main()
 {
   ll g, l;
   while (scanf("%lld%lld", &g, &l) == 2) {
-    ll ab = g * l;
-    ll best = g * l;
-    ll best_a, best_b;
-    //printf("%lld\n", best);
-    for (ll i = 1; i*i <= ab; i++) {
-      ll q = ab/i;
-      ll r = ab%i;
-      //printf("%lld / %lld = %lld ... %lld\n", ab, i, q, r);
-      if (r == 0 && (i + q < best)) {
-        best = i + q;
-        best_a = i;
-        best_b = q;
+    ll mul = l / g;
+    ll best = 1LL << 62;
+    ll a, b = 0;
+    for (ll i = 2; i*i <= mul; i++) {
+      // printf("i: %lld\n", i);
+      if (mul % i == 0) {
+        divisors.push_back(i); divisors.push_back(mul / i);
+        if (i + mul/i < best) { a = i; b = mul/i; best = a + b; }
       }
     }
-    if (best_a > best_b) swap(best_a, best_b);
-    printf("%lld %lld\n", best_a, best_b);
+    if (a > b) swap(a, b);
+    printf("%lld %lld\n", a*g, b*g);
   }
 }
