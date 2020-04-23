49bff2eec88145b00612f2d782d028f1743d7789
aoibird <aoi@aoibird.com>
2020-04-23 10:22:32 +0800

    Submit CPC Exercise 2-49, GCD & LCM Inverse, POJ 2429 (TLE)

--- cpc/exercise2-49_gcdlcminverse.cpp
+++ cpc/exercise2-49_gcdlcminverse.cpp
@@ -10,21 +10,17 @@
 using namespace std;
 typedef long long ll;
 
-vector<int> divisors;
-
 int main()
 {
   ll g, l;
   while (scanf("%lld%lld", &g, &l) == 2) {
     ll mul = l / g;
-    ll best = 1LL << 62;
-    ll a, b = 0;
-    for (ll i = 2; i*i <= mul; i++) {
+    //ll best = 1LL << 62;
+    ll a = 0, b = 0;
+    // printf("%lld\n", (ll)sqrt(mul));
+    for (ll i = (ll)sqrt((long double)mul); i >= 2; i--) {
       // printf("i: %lld\n", i);
-      if (mul % i == 0) {
-        divisors.push_back(i); divisors.push_back(mul / i);
-        if (i + mul/i < best) { a = i; b = mul/i; best = a + b; }
-      }
+      if (mul % i == 0) { a = i; b = mul/i; break; }
     }
     if (a > b) swap(a, b);
     printf("%lld %lld\n", a*g, b*g);
