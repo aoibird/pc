1a6fcdfdd91d962e211585c291200b8005bb756f
aoibird <aoi@aoibird.com>
2020-04-25 21:31:13 +0800

    Submit AOJ NTL_1_A, Prime Factorize, AOJ NTL_1_A (MLE)

--- aoj/ntl_1_a_primefactorize.cpp
+++ aoj/ntl_1_a_primefactorize.cpp
@@ -10,30 +10,25 @@ using namespace std;
 
 const int MAXN = 1000000000+10;
 bool V[MAXN];
-// vector<int> prime;
+vector<int> prime;
 vector<int> factor;
 
 int main()
 {
   int n; scanf("%d", &n);
   int on = n;
-  for (int i = 2; i*i <= on; i++) {
+  for (int i = 2; i <= on && n != 1; i++) {
     if (!V[i]) {
-      // prime.push_back(i);
-      for (int j=i*i; j <= n; j += i) {
-        V[j] = true;
-      }
-      // printf("%d %d\n", i, n);
+      prime.push_back(i);
       while (n % i == 0) { factor.push_back(i); n /= i; }
     }
+    for (int j = 0; j < prime.size() && i*prime[j] <= on; j++) {
+      V[i * prime[j]] = true;
+      if (i % prime[j] == 0) break;
+    }
   }
-  // for (int i = 0; i <= n; i++) printf("%d:%d%c", i, V[i], i==n?'\n':' ');
-  if (factor.empty()) {
-    printf("%d: %d\n", on, on);
-  }
-  else {
-    printf("%d:", on);
-    for (int i = 0; i < factor.size(); i++) printf(" %d", factor[i]);
-    printf("\n");
-  }
+
+  printf("%d:", on);
+  for (int i = 0; i < factor.size(); i++) printf(" %d", factor[i]);
+  printf("\n");
 }
