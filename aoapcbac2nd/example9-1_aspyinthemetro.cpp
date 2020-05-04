556eb3d58dc499ff3464d8a69668ecba8ad8ad5a
aoibird <aoi@aoibird.com>
2020-05-04 20:09:19 +0800

    Submit AOAPC BAC 2nd Example 9-1, A Spy in the Metro, UVa 1025 (AC)

--- aoapcbac2nd/example9-1_aspyinthemetro.cpp
+++ aoapcbac2nd/example9-1_aspyinthemetro.cpp
@@ -8,9 +8,9 @@
 using namespace std;
 
 const int INF = 1000000;
-const int MAXN = 50+10;
-const int MAXM = 50+10;
-const int MAXT = 200+10;
+const int MAXN = 50*3;
+const int MAXM = 50*3;
+const int MAXT = 200*3;
 int times[MAXN], start0[MAXM], start1[MAXM];
 int N, T, m1, m2;
 int dp[MAXT][MAXN];
@@ -28,49 +28,38 @@ void input()
     for (int i = 0; i < m2; i++) scanf("%d", &start1[i]);
 }
 
-void print()
-{
-    printf("%d %d %d %d\n", N, T, m1, m2);
-    for (int i = 0; i < m1; i++) printf("%d ", start0[i]); printf("\n");
-    for (int i = 0; i < m2; i++) printf("%d ", start1[i]); printf("\n");
-}
-
 void solve()
 {
     for (int i = 0; i < m1; i++) {
         int time = start0[i];
-        // printf("start time %d ", start0[i]);
         has_train[time][1][0] = true;
         for (int j = 2; j <= N; j++) {
             time += times[j-1];
             has_train[time][j][0] = true;
-            // printf("%d ", time);
         }
-        // printf("\n");
     }
     for (int i = 0; i < m2; i++) {
         int time = start1[i];
-        // printf("start time %d ", start1[i]);
         has_train[time][N][1] = true;
         for (int j = N-1; j >= 1; j--) {
             time += times[j];
             has_train[time][j][1] = true;
-            // printf("%d ", time);
         }
-        // printf("\n");
     }
 
-    for (int i = 0; i <= N-1; i++) dp[T][i] = INF;
-    dp[T][N] = 0;
-    for (int i = T-1; i >= 0; i--) {
+    for (int i = 0; i <= T; i++)
+        for (int j = 1; j <= N; j++) dp[i][j] = INF;
+    dp[0][1] = 0;
+    for (int i = 0; i <= T; i++) {
         for (int j = 1; j <= N; j++) {
-            dp[i][j] = dp[i+1][j] + 1;
+            dp[i+1][j] = min(dp[i+1][j], dp[i][j] + 1);
             if (j < N && has_train[i][j][0] && i+times[j] <= T)
-                dp[i][j] = min(dp[i][j], dp[i+times[j]][j+1]);
+                dp[i+times[j]][j+1] = min(dp[i+times[j]][j+1], dp[i][j]);
             if (j > 1 && has_train[i][j][1] && i+times[j-1] <= T)
-                dp[i][j] = min(dp[i][j], dp[i+times[j-1]][j-1]);
+                dp[i+times[j-1]][j-1] = min(dp[i+times[j-1]][j-1], dp[i][j]);
         }
     }
+
 
 }
 
@@ -80,11 +69,10 @@ int main()
     while (scanf("%d", &N) == 1) {
         if (N == 0) break;
         input();
-        // print();
         solve();
         printf("Case Number %d: ", tc);
-        if (dp[0][1] >= INF) printf("impossible\n");
-        else printf("%d\n", dp[0][1]);
+        if (dp[T][N] >= INF) printf("impossible\n");
+        else printf("%d\n", dp[T][N]);
         tc++;
     }
 }
