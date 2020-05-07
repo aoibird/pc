dd18920eabdeb20999fa6e9efa348d9efac93abd
aoibird <aoi@aoibird.com>
2020-05-07 13:55:17 +0800

    Submit AOAPC BAC 2nd Example 9-5, Jin Ge Jin Qu [h]ao, UVa 12563 (WA)

---
+++ aoapcbac2nd/example9-5_jingejinquhao.cpp
@@ -0,0 +1,64 @@
+// UVa 12563
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 50 + 10;
+const int MAXT = 180 * MAXN + 678;
+int A[MAXN];
+int N, T;
+int dp[MAXN][MAXT];
+int w[MAXN][MAXT];
+
+void input()
+{
+    scanf("%d%d", &N, &T);
+    for (int i = 1; i <= N; i++) scanf("%d", &A[i]);
+}
+
+void print_table()
+{
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j < T; j++) {
+            printf("%d%c", dp[i][j], j==T-1?'\n':' ');
+        }
+    }
+    printf("\n");
+}
+
+void solve()
+{
+    // printf("N = %d T = %d\n", N, T);
+    // for (int i = 1; i <= N; i++) printf("%d ", A[i]); printf("\n");
+    for (int i = 1; i <= N; i++) {
+        for (int j = A[i]; j < T; j++) {
+            if (dp[i-1][j] > dp[i-1][j-A[i]] + 1) {
+                dp[i][j] = dp[i-1][j];
+                w[i][j] = w[i-1][j];
+            }
+            else {
+                dp[i][j] = dp[i-1][j-A[i]] + 1;
+                w[i][j] = w[i-1][j-A[i]] + A[i];
+            }
+        }
+    }
+    // print_table();
+}
+
+int main()
+{
+    int TC; scanf("%d", &TC);
+    for (int i = 1; i <= TC; i++) {
+        memset(dp, 0, sizeof(dp));
+        memset(w, 0, sizeof(w));
+        input();
+        solve();
+
+        printf("Case %d: %d %d\n", i, dp[N][T-1]+1, w[N][T-1]+678);
+    }
+}
