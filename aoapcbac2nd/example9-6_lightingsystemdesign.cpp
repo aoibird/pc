97a7f38e20a99adca7d74e3cba7bfdc2889a309c
aoibird <aoi@aoibird.com>
2020-05-07 16:13:51 +0800

    Submit AOAPC BAC 2nd Example 9-6, Lighting System Design, UVa 11400 (AC)

--- aoapcbac2nd/example9-6_lightingsystemdesign.cpp
+++ aoapcbac2nd/example9-6_lightingsystemdesign.cpp
@@ -8,12 +8,22 @@
 #include <algorithm>
 using namespace std;
 
+struct Lamp {
+    int V, K, C, L;
+    Lamp(int iv=0, int ik=0, int ic=0, int il=0) { V=iv; K=ik; C=ic; L=il; }
+};
+
 const int INF = 1000000000;
 const int MAXN = 1000+10;
-int V[MAXN], K[MAXN], C[MAXN], L[MAXN];
+Lamp A[MAXN];
 int S[MAXN];
 int N;
 int dp[MAXN];
+
+bool compare(const Lamp &a, const Lamp &b)
+{
+    return a.V < b.V;
+}
 
 void print_table()
 {
@@ -23,14 +33,14 @@ void print_table()
 
 void solve()
 {
-    S[0] = L[0];
-    for (int i = 1; i < N; i++) S[i] = S[i-1] + L[i];
+    S[0] = A[0].L;
+    for (int i = 1; i < N; i++) S[i] = S[i-1] + A[i].L;
     for (int i = 0; i < N; i++) dp[i] = INF;
 
     for (int i = 0; i < N; i++) {
-        dp[i] = K[i] + S[i] * C[i];
+        dp[i] = A[i].K + S[i] * A[i].C;
         for (int j = 0; j < i; j++) {
-            dp[i] = min(dp[i], dp[j] + (S[i] - S[j]) * C[i] + K[i]);
+            dp[i] = min(dp[i], dp[j] + (S[i] - S[j]) * A[i].C + A[i].K);
         }
     }
     // print_table();
@@ -41,9 +51,12 @@ void solve()
 int main()
 {
     while (scanf("%d", &N) == 1) {
+        memset(dp, 0, sizeof(dp));
         if (N == 0) break;
-        for (int i = 0; i < N; i++)
-            scanf("%d%d%d%d", &V[i], &K[i], &C[i], &L[i]);
+        for (int i = 0; i < N; i++) {
+            scanf("%d%d%d%d", &A[i].V, &A[i].K, &A[i].C, &A[i].L);
+        }
+        sort(A, A+N, compare);
         solve();
     }
 }
