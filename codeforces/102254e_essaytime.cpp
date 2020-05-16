ec45ca89539887f24e33dd1e034bc770661466c1
aoibird <aoi@aoibird.com>
2020-05-16 11:57:51 +0800

    Submit Codeforces 102254 E, Essay Time, Codeforces 102254 E (RE)

--- codeforces/102254e_essaytime.cpp
+++ codeforces/102254e_essaytime.cpp
@@ -1,28 +1,38 @@
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <map>
+#include <unordered_map>
 using namespace std;
 
-map<string, int> m;
-vector<string> v;
-const int MAXL = 8000000+10;
+const int MAXL = 800000+10;
+
+map<string,int> m;
+vector<int> v;
+string words[MAXL];
+char cha[MAXL];
 
 int main()
 {
   int n; scanf("%d", &n);
-  string s;
-  char str[MAXL];
   int cnt = 0;
   for (int i = 0; i < n; i++) {
-    scanf("%s", str);
-    s = string(str);
-    m[s] += 1;
-    if (m[s] > 1) { v.push_back(s); cnt++; }
+      scanf("%s", cha);
+      words[i] = string(cha);
+      m[words[i]] += 1;
+    if (m[words[i]] > 1) { v.push_back(i); cnt++; }
   }
 
   if (cnt == 0) printf("SAFO\n");
   else {
       printf("%d\n", cnt);
-      for (int i = 0; i < v.size(); i++) cout << v[i] << "\n";
+      for (int i = 0; i < v.size(); i++) {
+          printf("%s\n", words[v[i]].c_str());
+      }
   }
 }
