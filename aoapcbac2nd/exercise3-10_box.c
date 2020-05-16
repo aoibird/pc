54eec51077ceaf290fd466dd1c3113f9e2b43c18
aoibird <aoi@aoibird.com>
2020-05-16 10:52:28 +0800

    Submit AOAPC BAC 2nd Exercise 3-10, Box, UVa 1587 (WA)

--- aoapcbac2nd/exercise3-10_box.c
+++ aoapcbac2nd/exercise3-10_box.c
@@ -1,76 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-int a[12] = {0};
-int set[12] = {0};
-int cnt[12] = {0};
-int n = 0;
-
-void trace()
-{
-  for (int i = 0; i < n; i++) {
-    printf("%d ", set[i]);
-  }
-  printf("\n");
-  for (int i = 0; i < n; i++) {
-    printf("%d ", cnt[i]);
-  }
-  printf("\n");
-}
+int W[6];
+int H[6];
+int V[6];
 
 int solve()
 {
-  for (int i = 0; i < 12; i++) {
-    int x = a[i];
-    int include = 0;
-    for (int j = 0; j < n; j++) {
-      if (x == set[j]) {
-        include = 1;
-        cnt[j] += 1;
-      }
-    }
-
-    if (include == 0) {
-      set[n] = x;
-      cnt[n] = 1;
-      n += 1;
-    }
-
-    //trace();
+  for (int i = 0; i < 6; i++) {
+    if (V[i]) continue;
+    int pair = 0;
+    for (int j = 0; j < 6; j++)
+      if (i!=j && W[i]==W[j] && H[i]==H[j]) { V[i]=V[j]=1; pair=1; break; }
+    if (pair == 0) return 0;
   }
-
-
-  if (n != 3) {
-    return 0;
-  }
-  else {
-    for (int i = 0; i < n; i++) {
-      if (cnt[i] != 4) {
-        return 0;
-      }
-    }
-    return 1;
-  }
+  return 1;
 }
 
 int main()
 {
-  while (1) {
-    // input
-    memset(a, 0, sizeof(a));
-    memset(cnt, 0, sizeof(cnt));
-    memset(set, 0, sizeof(set));
-    n = 0;
-    for (int i = 0; i < 12; i++) {
-      if (scanf("%d", &a[i]) != 1) {
-        return 0;
-      }
+  while (scanf("%d%d", &W[0], &H[0]) == 2) {
+    for (int i = 1; i < 6; i++) {
+      scanf("%d%d", &W[i], &H[i]);
     }
+    for (int i = 0; i < 6; i++)
+      if (H[i] > W[i]) { int temp = W[i]; W[i] = H[i]; H[i] = temp; }
 
-    // solve
     int result = solve();
 
     if (result) printf("POSSIBLE\n");
     else printf("IMPOSSIBLE\n");
+    memset(W, 0, sizeof(W));
+    memset(H, 0, sizeof(H));
+    memset(V, 0, sizeof(V));
   }
 }
