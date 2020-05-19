52833493244fd4a550f3b77761108bb8c59573e2
aoibird <aoi@aoibird.com>
2020-05-19 17:05:35 +0800

    Submit Codeforces 414 B, Mashmokh and ACM, Codeforces 414 B (AC)

--- codeforces/414b_mashmokhandacm.cpp
+++ codeforces/414b_mashmokhandacm.cpp
@@ -5,14 +5,17 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
 const int MAXN = 2000+10;
 const int MAXK = 2000+10;
+const int mod = 1000000007;
 int N, K;
 int dp[MAXK][MAXN];
+vector<int> f[MAXN];
 
 void print_table()
 {
@@ -25,25 +28,40 @@ void print_table()
     }
 }
 
+void factor(int x)
+{
+    int i;
+    for (i = 1; i*i < x; i++) {
+        if (x % i == 0) { f[x].push_back(i); f[x].push_back(x / i); }
+    }
+    if (i*i == x) f[x].push_back(i);
+    sort(f[x].begin(), f[x].end());
+}
+
 void solve()
 {
     for (int i = 1; i <= N; i++) dp[1][i] = 1;
     for (int i = 2; i <= K; i++) {
         for (int j = 1; j <= N; j++) {
-            for (int k = 1; k <= N; k++) {
-                if (j % k == 0) dp[i][j] += dp[i-1][k];
+            for (int k = 0; k < f[j].size(); k++) {
+                dp[i][j] = dp[i][j] + dp[i-1][f[j][k]];
+                if (dp[i][j] >= mod) dp[i][j] -= mod;
             }
         }
     }
     // print_table();
 
     int sum = 0;
-    for (int j = 0; j <= N; j++) sum += dp[K][j];
+    for (int j = 0; j <= N; j++) {
+        sum = sum + dp[K][j];
+        if (sum >= mod) sum -= mod;
+    }
     printf("%d\n", sum);
 }
 
 int main()
 {
+    for (int i = 1; i <= MAXN; i++) factor(i);
     while (scanf("%d%d", &N, &K) == 2) {
         memset(dp, 0, sizeof(dp));
 
