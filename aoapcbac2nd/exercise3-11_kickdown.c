64ecb25590f62b96c67ad6dce7fd5c6c7623b046
aoibird <aoi@aoibird.com>
2020-06-27 16:53:36 +0800

    Submit AOAPC BAC 2nd Exercise 3-11, Kickdown, UVa 1588 (AC)

--- aoapcbac2nd/exercise3-11_kickdown.c
+++ aoapcbac2nd/exercise3-11_kickdown.c
@@ -1,88 +1,53 @@
+// UVa 1588
 #include <stdio.h>
+#include <string.h>
 #define MAXS 105
+#define INF 1000
+#define CEN 150
 
-int s1[MAXS];
-int s2[MAXS];
+char s1[MAXS];
+char s2[MAXS];
 int n1, n2;
 
-void trace()
+int get_height(int a, int b, int c, int d)
 {
-  for (int i = 0; i < n1; i++) {
-    printf("%d ", s1[i]);
-  }
-  printf("\n");
-  for (int i = 0; i < n2; i++) {
-    printf("%d ", s2[i]);
-  }
-  printf("\n\n");
-}
-
-void input()
-{
-  char c;
-  n1 = 0, n2 = 0;
-  while ((c = getchar()) != '\n') {
-    s1[n1++] = (char) c - '0';
-  }
-  //trace();
-  while ((c = getchar()) != '\n') {
-    s2[n2++] = (char) c - '0';
-  }
-
-  //trace();
+    int f = (a < c) ? c : a;
+    int t = (b < d) ? b : d;
+    int offset1 = f - c;
+    int offset2 = f - a;
+    int len = t - f;
+    int h = 2;
+    for (int i = 0; i < len; i++) {
+        int c1 = s1[offset1 + i];
+        int c2 = s2[offset2 + i];
+        if (c1 == '1' && c2 == '1') h = h > 2 ? h : 2;
+        else if (c1 == '2' && c2 == '2') h = h > 4 ? h : 4;
+        else h = h > 3 ? h : 3;
+        /* printf("   c1, c2, h %c %c %d\n", c1, c2, h); */
+    }
+    /* printf("f, t: %d %d offset1, offset2: %d %d len: %d\n", */
+    /*        f, t, offset1, offset2, len); */
+    if (h > 3) return INF;
+    else return n1 + n2 - len;
 }
 
 void solve()
 {
-  int *p = s1, *q = s2;
-  if (n1 > n2) {
-    int temp = n1;
-    n1 = n2;
-    n2 = temp;
-    int *t = p;
-    p = q;
-    q = t;
-  }
-
-  int left, right;
-  int min = n1 + n2;
-  int t = min;
-  for (left = -n1; left < n2; left++) {
-    //printf("left: %d\n", left);
-    right = left + n1;
-    int ok = 1;
-    for (int x = 0; x < right; x++) {
-      //printf("s1[x] s2[x-left] %d %d\n", s1[x], s2[x-left]);
-      if (s1[x] + s2[x-left] > 3) {
-        ok = 0;
-      }
+    n1 = strlen(s1);
+    n2 = strlen(s2);
+    int minl = n1 + n2;
+    for (int i = -n2; i <= n1 + n2; i++) {
+        int l = get_height(i, i+n2, 0, n1);
+        minl = (l < minl) ? l : minl;
+        /* printf("l = %d (%d %d %d %d)\n", l, i, i+n2, 0, n1); */
     }
-    //printf("ok %d\n", ok);
-    if (ok) {
-      if (left < 0) {
-        t = n2 - left;
-      }
-      else if (right < n2) {
-        t = n2;
-      }
-      else {
-        t = right;
-      }
-      if (t < min) min = t;
-    }
-
-    //printf("min: %d\n", min);
-  }
-  printf("%d\n", min);
-
+    printf("%d\n", minl);
 }
 
 int main()
 {
-  int c;
-  while ((c = getchar()) != EOF) {
-    ungetc(c, stdin);
-    input();
+  while (scanf("%s", s1) == 1) {
+    scanf("%s", s2);
     solve();
   }
 }
