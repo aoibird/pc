c19ce56fc276f2db1c12b99da70e62f3ccc525c5
aoibird <aoi@aoibird.com>
2020-06-27 15:25:37 +0800

    Submit AOAPC BAC 2nd Exercise 3-8, Repeating Decimals, UVa 202 (WA)

---
+++ aoapcbac2nd/exercise3-8_repeatingdecimals.cpp
@@ -0,0 +1,58 @@
+// UVa 202
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+
+int a, b; // <= 3000
+vector<int> Q;
+vector<int> R;
+map<int, int> RL;
+
+void solve()
+{
+    int left = 0, right = 0;
+    int integral = a / b;
+    int at = a % b * 10;
+    for (int i = 0; ; i++) {
+        Q.push_back(at / b);
+        R.push_back(at % b);
+        if (at % b == 0) { left = i+1; right = i+2; break; }
+        else if (RL.count(at % b) == 0) { RL[at % b] = i; }
+        else { left = RL[at % b]; right = i; break; }
+        at = (at % b) * 10;
+        // printf("a = %d\n", a);
+    }
+    // printf("=================================\n");
+    // printf("integral, left, right: %d %d %d\n", integral, left, right);
+    // for (int i = 0; i < Q.size(); i++)
+    //     printf("%d%c", Q[i], i==Q.size()-1?'\n':' ');
+    // for (int i = 0; i < R.size(); i++)
+    //     printf("%d%c", R[i], i==R.size()-1?'\n':' ');
+
+    printf("%d/%d = %d.", a, b, integral);
+    for (int i = 0; i < left; i++) printf("%d", Q[i]);
+    printf("(");
+    for (int i = left; i < right && i<50; i++) printf("%d", Q[i]);
+    printf("%s)\n", right-left>50?"...":"");
+    printf("   %d = number of digits in repeating cycle\n", right-left);
+
+}
+
+int main()
+{
+    while (scanf("%d%d", &a, &b) == 2) {
+        Q.resize(0);
+        R.resize(0);
+        RL.clear();
+        solve();
+    }
+}
