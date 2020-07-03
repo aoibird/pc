d6dc4558c1c5aebdc24c3d72f62ddecf299ca371
aoibird <aoi@aoibird.com>
2020-07-03 10:34:18 +0800

    Submit CPC Exercise 2-6, Meteor Shower, POJ 3669 (WA)

--- cpc/exercise2-6_meteorshower.cpp
+++ cpc/exercise2-6_meteorshower.cpp
@@ -9,71 +9,25 @@
 #include <queue>
 using namespace std;
 
-struct Meteor {
-  int time;
-  int row;
-  int col;
-  Meteor(int t=0, int r=0, int c=0) {
-    time = t;
-    row = r;
-    col = c;
-  }
-  bool operator<(const Meteor& m) const {
-    return time < m.time;
-  }
-};
-
 struct Node {
-  int r;
-  int c;
-  int d;
-  Node(int ri=0, int ci=0, int di=0) {
-    r = ri;
-    c = ci;
-    d = di;
-  }
-  bool operator<(const Node& n) const {
-    return d < n.d;
-  }
+  int r, c, d;
+  Node(int ri=0, int ci=0, int di=0) { r = ri; c = ci; d = di; }
 };
 
 const int INF = 100000;
-const int MAXR = 300;
-const int MAXC = 300;
-//const int MAXM = 50000;
-int GRID[MAXR+10][MAXC+10];
-int D[MAXR+10][MAXC+10];
-//Meteor METEORS[MAXM];
+const int MAXR = 300+10;
+const int MAXC = 300+10;
+int GRID[MAXR][MAXC];
+int D[MAXR][MAXC];
 int M;
 int T;
 const int dr[4] = {1, -1, 0, 0};
 const int dc[4] = {0, 0, 1, -1};
 
-void input()
-{
-  T = 0;
-  scanf("%d", &M);
-  for (int i = 0; i < M; i++) {
-    int r, c, t;
-    scanf("%d%d%d", &r, &c, &t);
-    T = max(T, t);
-    //METEORS[i] = Meteor(t, x, y);
-    GRID[r][c] = t;
-    for (int i = 0; i < 4; i++) {
-      int nr = r+dr[i];
-      int nc = c+dc[i];
-      if (nr >= 0 && nr < MAXR && nc >= 0 && nc < MAXC
-          && (t < GRID[nr][nc])) {
-        GRID[nr][nc] = t;
-      }
-    }
-  }
-}
-
 void print()
 {
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
       if (GRID[i][j] == INF) printf("# ");
       else printf("%d ", GRID[i][j]);
     }
@@ -81,43 +35,56 @@ void print()
   }
 }
 
+void input()
+{
+  T = 0;
+  for (int i = 0; i < M; i++) {
+    int r, c, t;
+    scanf("%d%d%d", &r, &c, &t);
+    T = max(T, t);
+    GRID[r][c] = t;
+    for (int j = 0; j < 4; j++) {
+      int nr = r+dr[j];
+      int nc = c+dc[j];
+      if (nr >= 0 && nr < MAXR && nc >= 0 && nc < MAXC
+        && (GRID[nr][nc] == 0 || t < GRID[nr][nc])) { GRID[nr][nc] = t; }
+    }
+  }
+}
+
+
 int solve()
 {
   queue<Node> q;
   q.push(Node(0, 0, 0));
   D[0][0] = 0;
-  for (int t = 0; t < T; t++) {
-
-    while (q.front().d == t) {
-      Node curr = q.front(); q.pop();
-      int r = curr.r;
-      int c = curr.c;
-      for (int i = 0; i < 4; i++) {
-        int nr = r+dr[i];
-        int nc = c+dc[i];
-        if (nr >= 0 && nr < MAXR && nc >= 0 && nc < MAXC
-            && t+1 < GRID[nr][nc] && D[nr][nc] == 0) {
-          q.push(Node(nr, nc, t+1));
-          D[nr][nc] = t+1;
-        }
+  while (!q.empty()) {
+    Node curr = q.front(); q.pop();
+    int r = curr.r;
+    int c = curr.c;
+    int d = curr.d;
+    if (GRID[r][c] == 0) { return d; }
+    for (int i = 0; i < 4; i++) {
+      int nr = r+dr[i];
+      int nc = c+dc[i];
+      int nd = d + 1;
+      if (nr >= 0 && nr < MAXR && nc >= 0 && nc < MAXC
+        && (GRID[nr][nc] == 0 || nd < GRID[nr][nc]) && D[nr][nc] == 0) {
+        q.push(Node(nr, nc, nd));
+        D[nr][nc] = nd;
       }
     }
   }
-
-  if (q.empty()) return -1;
-  else return q.front().d;
+  return -1;
 }
 
 int main()
 {
-  for (int i = 0; i < MAXR+10; i++) {
-    for (int j = 0; j < MAXC+10; j++) {
-      GRID[i][j] = INF;
-      D[i][j] = 0;
-    }
+  while (scanf("%d", &M) == 1) {
+    memset(GRID, 0, sizeof(GRID));
+    memset(D, 0, sizeof(D));
+    input();
+    int res = solve();
+    printf("%d\n", res);
   }
-  input();
-  int res = solve();
-  printf("%d\n", res);
-  //print();
 }
