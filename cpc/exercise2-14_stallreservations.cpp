7e081db73a8786ac675f4c9d5dec8cda2196934e
aoibird <aoi@aoibird.com>
2020-07-22 21:45:26 +0800

    Submit CPC Exercise 2-14, Stall Reservations, POJ 3190 (WA)

--- cpc/exercise2-14_stallreservations.cpp
+++ cpc/exercise2-14_stallreservations.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
+#include <cctype>
+#include <cmath>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 #include <queue>
-#define MAXN 50050
-#define MAXAB 1000100
 using namespace std;
 typedef pair<int,int> PII;
+#define MAXN 50050
+#define MAXAB 1000100
 
 struct cow {
   int index;
@@ -15,73 +20,53 @@ struct cow {
 };
 
 
-cow a[MAXN];
-int f[MAXN]; // finish
+cow A[MAXN];
 int N;
 
-
-void input()
+bool compare_finish(struct cow &a, struct cow &b)
 {
-  scanf("%d", &N);
-  for (int i = 0; i < N; i++) {
-    scanf("%d%d", &a[i].start, &a[i].finish);
-    a[i].index = i;
-  }
+    return a.finish < b.finish;
 }
 
-int cmpfinish(cow x, cow y)
+bool compare_index(struct cow &a, struct cow &b)
 {
-  if (x.start < y.start) return 1;
-  else return 0;
-}
-
-int cmpindex(cow x, cow y)
-{
-  if (x.index < y.index) return 1;
-  else return 0;
+    return a.index < b.index;
 }
 
 void solve()
 {
-  sort(a, a+N, cmpfinish);
-  priority_queue<PII, vector<PII>, greater<PII> > pq;
+  sort(A, A+N, compare_finish);
+  priority_queue<PII, vector<PII>, greater<PII>> pq; // (finish, index)
 
-  int cnt = 0;
   for (int i = 0; i < N; i++) {
-    PII t;
-    //if (!pq.empty()) printf("%d %d\n", pq.top().first, pq.top().second);
-    //else printf("0 0\n");
-    if (!pq.empty() && pq.top().first < a[i].start) { // old
-      t = pq.top(); pq.pop();
-      pq.push(PII(a[i].finish, t.second)); // update finish time of old stall
-      a[i].stall = t.second;
-      //printf("old: %d %d %d\n", a[i].start, a[i].finish, a[i].stall);
-    }
-    else { // new
-      cnt++;
-      pq.push(PII(a[i].finish, cnt)); // add new stall
-      a[i].stall = cnt;
-      //printf("new: %d %d %d\n", a[i].start, a[i].finish, a[i].stall);
-    }
-
+      int start = A[i].start, finish = A[i].finish;
+      if (pq.empty() || pq.top().first >= start) {
+          int size = pq.size();
+          pq.push(PII(finish, size)); A[i].stall = size;
+      }
+      else {
+          PII p = pq.top(); pq.pop();
+          A[i].stall = p.second;
+          pq.push(PII(A[i].finish, p.second));
+      }
   }
 
-  printf("%d\n", cnt);
-  sort(a, a+N, cmpindex);
+
+  printf("%d\n", (int)pq.size());
+  sort(A, A+N, compare_index);
   for (int i = 0; i < N; i++) {
-    printf("%d\n", a[i].stall);
+    printf("%d\n", A[i].stall+1);
   }
-
-  /*
-  for (int i = 1; i <= cnt; i++) {
-    printf("%d ", f[i]);
-  }
-  printf("\n");
-  */
 }
 
 int main()
 {
-  input();
-  solve();
+    while (scanf("%d", &N) == 1) {
+        for (int i = 0; i < N; i++) {
+            scanf("%d%d", &A[i].start, &A[i].finish);
+            A[i].index = i;
+        }
+        solve();
+    }
+
 }
