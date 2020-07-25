40ba9c8599612c8fd9ec19562fdadfbc98b6103e
aoibird <aoi@aoibird.com>
2020-07-25 15:05:49 +0800

    Submit CPC Exercise 2-17, Allowance, POJ 3040 (WA)

--- cpc/exercise2-17_allowance.cpp
+++ cpc/exercise2-17_allowance.cpp
@@ -6,13 +6,23 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
+const int INF = 1000000000;
 const int MAXN = 20+10;
 PII D[MAXN];
 int N, C;
+
+void print_d()
+{
+    for (int i = 0; i < N; i++) printf("(%d,%d)%c", D[i].first, D[i].second,
+                                       i==N-1?'\n':' ');
+}
 
 bool cmp(const PII &a, const PII &b)
 {
@@ -22,6 +32,7 @@ bool cmp(const PII &a, const PII &b)
 int take(int amount)
 {
     int cnt = 0;
+    map<int,int> m;
     // printf("take ---");
     while (amount > 0) {
         int x = -1;
@@ -34,25 +45,37 @@ int take(int amount)
         }
         if (x != -1) {
             // printf("%d(%d) ", D[x].first, amount);
-            amount -= D[x].first; D[x].second -= 1; cnt++;
+            amount -= D[x].first; D[x].second -= 1; m[x] += 1; cnt++;
         }
         else if (y != -1) {
             // printf("%d(%d) ", D[y].first, amount);
-            amount -= D[y].first; D[y].second -= 1; cnt++;
+            amount -= D[y].first; D[y].second -= 1; m[y] += 1; cnt++;
         }
         else break;
     }
-    // printf("\n");
-    if (cnt != 0) return true;
-    else return false;
+    int mi = INF;
+    for (map<int,int>::iterator it = m.begin(); it != m.end(); it++) {
+        PII pair = *it; int index = pair.first; int cnt = pair.second;
+        // printf("[%d %d %d]", D[index].second, cnt, D[index].second / cnt);
+        mi = min(mi, D[index].second / cnt);
+    }
+    for (map<int,int>::iterator it = m.begin(); it != m.end(); it++) {
+         PII pair = *it; int index = pair.first; int cnt = pair.second;
+         D[index].second -= (mi * cnt);
+    }
+    // printf(" * %d\n", mi+1);
+    // print_d();
+    if (cnt != 0) return mi+1;
+    else return 0;
 }
+
 
 void solve()
 {
     sort(D, D+N, cmp);
-    int cnt = 0;
-    while (take(C)) {
-        cnt++;
+    int cnt = 0, c = 0;
+    while ((c = take(C)) != 0) {
+        cnt += c;
     }
     printf("%d\n", cnt);
 }
