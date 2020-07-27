ff5fbb6da29a5f04684be2901d885a22a3c21cc2
aoibird <aoi@aoibird.com>
2020-07-27 16:24:04 +0800

    Submit CPC Exercise 2-26, Ant Counting, POJ 3046 (MLE)

--- cpc/exercise2-26_antcounting.cpp
+++ cpc/exercise2-26_antcounting.cpp
@@ -12,40 +12,39 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
-const int MAXA = 1e5+20;
-int ANTS[MAXA];
+const int MAXA = 1e5+20; // ants
+const int MAXT = 1e3+10; // types
+const int MAXS = MAXA;   // sizes
+const int M = 1e6;
+int U[MAXT];
+int dp[MAXT][MAXS];
 int T, A, S, B;
-queue<int> Q;
+
+void print_array(int *a, int n)
+{
+    for (int i = 0; i < n; i++) printf("%d%c", a[i], i==n-1?'\n':' ');
+}
+
 
 int main()
 {
     vector<int> t;
     while (scanf("%d%d%d%d", &T, &A, &S, &B) == 4) {
-        while (!Q.empty()) Q.pop();
-        for (int i = 0; i < A; i++) scanf("%d", &ANTS[i]);
-        sort(ANTS, ANTS+A);
+        memset(U, 0, sizeof(U));
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i < A; i++) { int a; scanf("%d", &a); U[a-1] += 1; }
+        // print_array(U, A);
 
-        int cnt = 0;
-        for (int i = 0; i <= B && i <= A; i++) {
-            // printf("[%d] %d\n", i, (int)Q.size());
-            if (i >= S && i <= B) cnt += Q.size();
-            if (Q.empty()) {
-                for (int i = 0; i < A; i++) {
-                    if (i == 0 || ANTS[i] != ANTS[i-1]) { Q.push(i); }
-                }
-
-            }
-            else {
-                t.resize(0);
-                while (!Q.empty()) {
-                    int x = Q.front(); Q.pop();
-                    for (int i = x+1; i < A; i++) {
-                        if (i == x+1 || ANTS[i] != ANTS[i-1]) { t.push_back(i); }
-                    }
-                }
-                for (int i = 0; i < t.size(); i++) Q.push(t[i]);
+        int sum = 0;
+        for (int i = 0; i <= T; i++) dp[i][0] = 1;
+        for (int i = 0; i < T; i++) { // types
+            for (int j = 1; j <= B; j++) { // sizes
+                if (j-1-U[i] >= 0) dp[i+1][j] = (dp[i+1][j-1] + dp[i][j] - dp[i][j-1-U[i]] + M) % M;
+                else dp[i+1][j] = (dp[i+1][j-1] + dp[i][j]) % M;
             }
         }
-        printf("%d\n", cnt);
+        // print_array(dp[T], B+1);
+        for (int j = S; j <= B; j++) sum = (sum + dp[T][j]) % M;
+        printf("%d\n", sum);
     }
 }
