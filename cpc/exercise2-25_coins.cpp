9761f575b0e41886f29d437a6f7531c3c4819e39
aoibird <aoi@aoibird.com>
2020-07-27 09:37:16 +0800

    Submit CPC Exercise 2-25, Coins, POJ 1742 (MLE)

--- cpc/exercise2-25_coins.cpp
+++ cpc/exercise2-25_coins.cpp
@@ -6,15 +6,17 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <set>
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
 const int MAXN = 100+10;
-const int MAXM = 1e5+10;
+//const int MAXM = 1e5+10;
 int A[MAXN];
 int C[MAXN];
-bool dp[MAXM];
+set<int> dp;
 int N, M;
 
 void print_array(bool a[MAXN], int n) {
@@ -24,30 +26,25 @@ void print_array(bool a[MAXN], int n) {
 
 void solve()
 {
-    dp[0] = true;
-    int upper = 0;
+    dp.insert(0);
     for (int i = 0; i < N; i++) {
         int a = A[i], c = C[i];
-        for (int j = 0; j <= upper && j <= M - a*c; j++) {
-            if (dp[j]) {
-                for (int k = 1; k <= c; k++) { dp[j+a*k] = true; }
-            }
+        vector<int> t; t.resize(0);
+        for (set<int>::iterator it = dp.begin(); it != dp.end(); it++) {
+            for (int k = 1; k <= c && *it + a*k <= M; k++) t.push_back(*it + a*k);
         }
-        for (int k = 1; k <= c && a*k <= M; k++) { dp[a*k] = true; }
-        upper += a*c;
-        // printf("upper = %d\n", upper);
+        for (int i = 0; i < t.size(); i++) dp.insert(t[i]);
     }
-    // print_array(dp, upper + A[N-1]*C[N-1]);
-    int cnt = 0;
-    for (int i = 1; i <= upper + A[N-1]*C[N-1] && i <= M; i++) if (dp[i]) cnt++;
-    printf("%d\n", cnt);
+    // for (set<int>::iterator it = dp.begin(); it != dp.end(); it++) { printf("%d ", *it); }
+    // printf("\n");
+    printf("%d\n", (int)dp.size()-1);
 }
 
 int main()
 {
     while (scanf("%d%d", &N, &M) == 2) {
         if (N == 0 && M == 0) break;
-        memset(dp, 0, sizeof(dp));
+        dp.clear();
         for (int i = 0; i < N; i++) { scanf("%d", &A[i]); }
         for (int i = 0; i < N; i++) { scanf("%d", &C[i]); }
         solve();
