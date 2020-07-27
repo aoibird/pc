05032b4379380933119b84ea31f1bf83b253f026
aoibird <aoi@aoibird.com>
2020-07-27 10:02:15 +0800

    Submit CPC Exercise 2-25, Coins, POJ 1742 (AC)

--- cpc/exercise2-25_coins.cpp
+++ cpc/exercise2-25_coins.cpp
@@ -6,17 +6,16 @@
 #include <cstring>
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#include <set>
 using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
 const int MAXN = 100+10;
-//const int MAXM = 1e5+10;
+const int MAXM = 1e5+10;
 int A[MAXN];
 int C[MAXN];
-set<int> dp;
+bool dp[MAXM];
+int v[MAXM];
 int N, M;
 
 void print_array(bool a[MAXN], int n) {
@@ -26,25 +25,26 @@ void print_array(bool a[MAXN], int n) {
 
 void solve()
 {
-    dp.insert(0);
+    dp[0] = true;
+    int cnt = 0;
     for (int i = 0; i < N; i++) {
         int a = A[i], c = C[i];
-        vector<int> t; t.resize(0);
-        for (set<int>::iterator it = dp.begin(); it != dp.end(); it++) {
-            for (int k = 1; k <= c && *it + a*k <= M; k++) t.push_back(*it + a*k);
+        memset(v, 0, sizeof(v));
+        for (int j = a; j <= M; j++) {
+            if (!dp[j] && dp[j-a] && v[j-a] < c) {
+                dp[j] = true; v[j] = v[j-a]+1; cnt++;
+            }
         }
-        for (int i = 0; i < t.size(); i++) dp.insert(t[i]);
     }
-    // for (set<int>::iterator it = dp.begin(); it != dp.end(); it++) { printf("%d ", *it); }
-    // printf("\n");
-    printf("%d\n", (int)dp.size()-1);
+    // print_array(dp, upper + A[N-1]*C[N-1]);
+    printf("%d\n", cnt);
 }
 
 int main()
 {
     while (scanf("%d%d", &N, &M) == 2) {
         if (N == 0 && M == 0) break;
-        dp.clear();
+        memset(dp, 0, sizeof(dp));
         for (int i = 0; i < N; i++) { scanf("%d", &A[i]); }
         for (int i = 0; i < N; i++) { scanf("%d", &C[i]); }
         solve();
