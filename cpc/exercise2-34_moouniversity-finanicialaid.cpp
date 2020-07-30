2cbf7a74f832d211baa81d39b510cacb13a3b5ee
aoibird <aoi@aoibird.com>
2020-07-30 21:04:28 +0800

    Submit CPC Exercise 2-34, Moo University - Finanicial Aid, POJ 2010 (WA)

--- cpc/exercise2-34_moouniversity-finanicialaid.cpp
+++ cpc/exercise2-34_moouniversity-finanicialaid.cpp
@@ -20,7 +20,8 @@ bool cmp_score(const PII &a, const PII &b) { return a.first > b.first; }
 bool cmp_aid(const PII &a, const PII &b) { return a.second < b.second; }
 
 priority_queue<PII, vector<PII>, greater<PII> > SEL; // score_i < score_j
-priority_queue<PII, vector<PII> > CAN; // score_i > score_j
+priority_queue<PII, vector<PII>, greater<PII> > CAN; // aid_i < aid_j
+vector<PII> temp;
 
 
 void solve()
@@ -29,20 +30,29 @@ void solve()
     sort(CA, CA+C, cmp_aid);
     for (int i = 0; i < N; i++) { SEL.push(CA[i]); sum += CA[i].second; }
     if (sum > F) { printf("-1\n"); return; }
-    for (int i = N; i < C; i++) { CAN.push(CA[i]); }
+    for (int i = N; i < C; i++) { CAN.push(PII(CA[i].second, CA[i].first)); }
     for (int i = 0; i < N/2; i++) { SEL.pop(); }
+    // while (!CAN.empty()) {
+    //     printf("(%d %d) ", CAN.top().first, CAN.top().second);
+    //     CAN.pop();
+    // }
+    // printf("\n");
+    temp.resize(0);
     while (!CAN.empty()) {
         PII cur = SEL.top(); int cur_score = cur.first, cur_aid = cur.second;
-        PII tar = CAN.top(); int tar_score = tar.first, tar_aid = tar.second;
+        PII tar = CAN.top(); int tar_score = tar.second, tar_aid = tar.first;
         // printf("current: (%d %d) target: (%d %d)\n",
         //        cur_score, cur_aid, tar_score, tar_aid);
-        if (tar_score < cur_score) { CAN.pop(); }
-        else {
-            if (tar_aid < cur_aid || sum - cur_aid + tar_aid <= F) {
-                SEL.pop(); SEL.push(tar); CAN.pop();
-            }
-            else { CAN.pop(); }
+        if (tar_score < cur_score) {
+            CAN.pop();
         }
+        else if (tar_score >= cur_score && sum - cur_aid + tar_aid <= F) {
+            SEL.pop(); SEL.push(PII(tar_score, tar_aid)); CAN.pop();
+            sum = sum - cur_aid + tar_aid;
+            for (int i = 0; i < temp.size(); i++) CAN.push(temp[i]);
+            temp.resize(0);
+        }
+        else { CAN.pop(); temp.push_back(tar); }
     }
     printf("%d\n", SEL.top().first);
 }
