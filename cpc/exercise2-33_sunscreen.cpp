0ed7429bc422c1e5a501dde5ab7735776afa6ddb
aoibird <aoi@aoibird.com>
2020-07-30 18:55:28 +0800

    Submit CPC Exercise 2-33, Sunscreen, POJ 3614 (WA)

--- cpc/exercise2-33_sunscreen.cpp
+++ cpc/exercise2-33_sunscreen.cpp
@@ -29,14 +29,17 @@ void solve()
     sort(LO, LO+L);
     int i = 0, cnt = 0;
     while (!PQ.empty() && i < L) {
-        PII p = PQ.top(); PQ.pop();
-        int lower = p.second, upper = p.first;
-        int &spf = LO[i].second, &cover = LO[i].first;
-        if (spf != 0 && lower <= cover && cover <= upper) {
-            spf -= 1; cnt++;
+        PII p = PQ.top();
+        int lower = p.first, upper = p.second;
+        int &spf = LO[i].first, &cover = LO[i].second;
+        if (lower <= spf) {
+            PQ.pop();
+            if (cover != 0 && spf <= upper) {
+                cover -= 1; cnt++;
+            }
         }
 
-        if (spf == 0) { i++; }
+        if (cover == 0 || lower > spf) i++;
     }
     // print_array(LO, L);
     printf("%d\n", cnt);
@@ -47,7 +50,7 @@ int main()
     while (scanf("%d%d", &C, &L) == 2) {
         while (!PQ.empty()) PQ.pop();
         memset(LO, 0, sizeof(LO));
-        for (int i = 0; i < C; i++) { int l, u; scanf("%d%d", &l, &u); PQ.push(PII(u, l)); }
+        for (int i = 0; i < C; i++) { int l, u; scanf("%d%d", &l, &u); PQ.push(PII(l, u)); }
         for (int i = 0; i < L; i++) { scanf("%d%d", &LO[i].first, &LO[i].second); }
         solve();
     }
