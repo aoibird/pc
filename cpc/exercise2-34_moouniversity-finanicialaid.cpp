80164b16068c464fa783e61570235dd9fbd1e3c1
aoibird <aoi@aoibird.com>
2020-07-30 22:24:19 +0800

    Submit CPC Exercise 2-34, Moo University - Finanicial Aid, POJ 2010 (AC)

--- cpc/exercise2-34_moouniversity-finanicialaid.cpp
+++ cpc/exercise2-34_moouniversity-finanicialaid.cpp
@@ -17,58 +17,52 @@ struct Calf {
     Calf(int i=0, int s=0, int a=0) { index=i; score=s; aid=a; }
 };
 
-//const int INF = 2 * 1e9 + 10;
+// const int INF = 0x3f3f3f3f;
+const int INF = 2 * 1e9 + 10;
 const int MAXC = 1e5+10;
-Calf A[MAXC]; // (score, finanicial aid) score_i < score_j
-Calf S[MAXC]; // aid_i < aid_j
+PII A[MAXC]; // (score, finanicial aid) score_i < score_j
+int back[MAXC];
+int front[MAXC];
 int N, C, F;
+priority_queue<int> Q;
 
-bool cmp_score(const Calf &a, const Calf &b)
+void print_array(int *a, int n)
 {
-    return a.score < b.score || (a.score==b.score && a.aid < b.aid);
-}
-
-bool cmp_aid(const Calf &a, const Calf &b)
-{
-    return a.aid < b.aid || (a.aid==b.aid && a.index < b.index);
-}
-
-void print_array(Calf *a, int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("(%d %d %d)%c", a[i].index, a[i].score, a[i].aid, i==n-1?'\n':' ');
+    for (int i = 0; i < n; i++) printf("%d%c", a[i], i==n-1?'\n':' ');
 }
 
 
 void solve()
 {
-    sort(A, A+C, cmp_score);
-    for (int i = 0; i < C; i++) { A[i].index = i; }
-    for (int i = 0; i < C; i++) { S[i] = A[i]; }
-    sort(S, S+C, cmp_aid);
+    sort(A, A+C);
 
-    // print_array(A, C);
-    // print_array(S, C);
-    int bestm = 0;
+    int sum;
+    sum = 0;
+    while (!Q.empty()) Q.pop();
     for (int i = 0; i < C; i++) {
-        int index = A[i].index, aid = A[i].aid;
-        // printf("median %d %d %d\n", index, score, aid);
-        int sumback = 0, tback = 0;
-        for (int j = 0; j < C && tback < N/2; j++) {
-            if (S[j].index < index) { sumback += S[j].aid; tback++; }
-        }
-        if (tback != N/2) continue;
-        int sumfront = 0, tfront = 0;
-        for (int j = 0; j < C && tfront < N/2; j++) {
-            if (S[j].index > index) { sumfront += S[j].aid; tfront++; }
-        }
-        if (tfront != N/2) continue;
-        int sum = sumback+sumfront+aid;
-        // printf("sum %d = %d(%d) + %d(%d) + %d\n",
-        //        sum, sumback, tback, sumfront, tfront, aid);
-        if (sum <= F) { bestm = i; }
+        back[i] = (Q.size()==N/2) ? sum : INF;
+        Q.push(A[i].second);
+        sum += A[i].second;
+        if (Q.size() > N/2) { sum -= Q.top(); Q.pop(); }
     }
-    printf("%d\n", A[bestm].score);
+    while (!Q.empty()) Q.pop();
+    sum = 0;
+    for (int i = C-1; i >= 0; i--) {
+        front[i] = (Q.size()==N/2) ? sum : INF;
+        Q.push(A[i].second);
+        sum += A[i].second;
+        if (Q.size() > N/2) { sum -= Q.top(); Q.pop(); }
+    }
+
+    // print_array(back, C);
+    // print_array(front, C);
+
+    for (int i = C-1; i >= 0; i--) {
+        if (back[i] + front[i] + A[i].second <= F) {
+            printf("%d\n", A[i].first); return;
+        }
+    }
+    printf("-1\n");
 }
 
 int main()
@@ -76,7 +70,7 @@ int main()
     while (scanf("%d%d%d", &N, &C, &F) == 3) {
         for (int i = 0; i < C; i++) {
             int s, a; scanf("%d%d", &s, &a);
-            A[i] = Calf(0, s, a);
+            A[i] = PII(s, a);
         }
         solve();
     }
