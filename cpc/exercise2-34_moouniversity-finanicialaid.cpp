5d7fd35c3b3c1a7b64fe4c8a2cb0bf4a3dc06cd7
aoibird <aoi@aoibird.com>
2020-07-30 21:51:22 +0800

    Submit CPC Exercise 2-34, Moo University - Finanicial Aid, POJ 2010 (TLE)

--- cpc/exercise2-34_moouniversity-finanicialaid.cpp
+++ cpc/exercise2-34_moouniversity-finanicialaid.cpp
@@ -12,57 +12,72 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
+struct Calf {
+    int index, score, aid;
+    Calf(int i=0, int s=0, int a=0) { index=i; score=s; aid=a; }
+};
+
+//const int INF = 2 * 1e9 + 10;
 const int MAXC = 1e5+10;
-PII CA[MAXC]; // (score, finanicial aid)
+Calf A[MAXC]; // (score, finanicial aid) score_i < score_j
+Calf S[MAXC]; // aid_i < aid_j
 int N, C, F;
 
-bool cmp_score(const PII &a, const PII &b) { return a.first > b.first; }
-bool cmp_aid(const PII &a, const PII &b) { return a.second < b.second; }
+bool cmp_score(const Calf &a, const Calf &b)
+{
+    return a.score < b.score || (a.score==b.score && a.aid < b.aid);
+}
 
-priority_queue<PII, vector<PII>, greater<PII> > SEL; // score_i < score_j
-priority_queue<PII, vector<PII>, greater<PII> > CAN; // aid_i < aid_j
-vector<PII> temp;
+bool cmp_aid(const Calf &a, const Calf &b)
+{
+    return a.aid < b.aid || (a.aid==b.aid && a.index < b.index);
+}
+
+void print_array(Calf *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("(%d %d %d)%c", a[i].index, a[i].score, a[i].aid, i==n-1?'\n':' ');
+}
 
 
 void solve()
 {
-    int sum = 0;
-    sort(CA, CA+C, cmp_aid);
-    for (int i = 0; i < N; i++) { SEL.push(CA[i]); sum += CA[i].second; }
-    if (sum > F) { printf("-1\n"); return; }
-    for (int i = N; i < C; i++) { CAN.push(PII(CA[i].second, CA[i].first)); }
-    for (int i = 0; i < N/2; i++) { SEL.pop(); }
-    // while (!CAN.empty()) {
-    //     printf("(%d %d) ", CAN.top().first, CAN.top().second);
-    //     CAN.pop();
-    // }
-    // printf("\n");
-    temp.resize(0);
-    while (!CAN.empty()) {
-        PII cur = SEL.top(); int cur_score = cur.first, cur_aid = cur.second;
-        PII tar = CAN.top(); int tar_score = tar.second, tar_aid = tar.first;
-        // printf("current: (%d %d) target: (%d %d)\n",
-        //        cur_score, cur_aid, tar_score, tar_aid);
-        if (tar_score < cur_score) {
-            CAN.pop();
+    sort(A, A+C, cmp_score);
+    for (int i = 0; i < C; i++) { A[i].index = i; }
+    for (int i = 0; i < C; i++) { S[i] = A[i]; }
+    sort(S, S+C, cmp_aid);
+
+    // print_array(A, C);
+    // print_array(S, C);
+    int bestm = 0;
+    for (int i = 0; i < C; i++) {
+        int index = A[i].index, aid = A[i].aid;
+        // printf("median %d %d %d\n", index, score, aid);
+        int sumback = 0, tback = 0;
+        for (int j = 0; j < C && tback < N/2; j++) {
+            if (S[j].index < index) { sumback += S[j].aid; tback++; }
         }
-        else if (tar_score >= cur_score && sum - cur_aid + tar_aid <= F) {
-            SEL.pop(); SEL.push(PII(tar_score, tar_aid)); CAN.pop();
-            sum = sum - cur_aid + tar_aid;
-            for (int i = 0; i < temp.size(); i++) CAN.push(temp[i]);
-            temp.resize(0);
+        if (tback != N/2) continue;
+        int sumfront = 0, tfront = 0;
+        for (int j = 0; j < C && tfront < N/2; j++) {
+            if (S[j].index > index) { sumfront += S[j].aid; tfront++; }
         }
-        else { CAN.pop(); temp.push_back(tar); }
+        if (tfront != N/2) continue;
+        int sum = sumback+sumfront+aid;
+        // printf("sum %d = %d(%d) + %d(%d) + %d\n",
+        //        sum, sumback, tback, sumfront, tfront, aid);
+        if (sum <= F) { bestm = i; }
     }
-    printf("%d\n", SEL.top().first);
+    printf("%d\n", A[bestm].score);
 }
 
 int main()
 {
     while (scanf("%d%d%d", &N, &C, &F) == 3) {
-        for (int i = 0; i < C; i++) scanf("%d%d", &CA[i].first, &CA[i].second);
-        while (!SEL.empty()) SEL.pop();
-        while (!CAN.empty()) CAN.pop();
+        for (int i = 0; i < C; i++) {
+            int s, a; scanf("%d%d", &s, &a);
+            A[i] = Calf(0, s, a);
+        }
         solve();
     }
 }
