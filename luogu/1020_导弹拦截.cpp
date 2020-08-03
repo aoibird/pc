4c2de0284fba8d2cb3f1d414e6e87a53af9b9113
aoibird <aoi@aoibird.com>
2020-08-03 16:07:15 +0800

    Submit Luogu 1020, 导弹拦截, Luogu 1020 (AC)

--- luogu/1020_导弹拦截.cpp
+++ luogu/1020_导弹拦截.cpp
@@ -13,6 +13,7 @@ typedef pair<int,int> PII;
 const int INF = 0x3f3f3f3f;
 const int MAXN = 1e5 + 10;
 int A[MAXN];
+int R[MAXN];
 int dp[MAXN];
 int N;
 
@@ -20,15 +21,17 @@ int main()
 {
     N = 0;
     while (scanf("%d", &A[N]) == 1) N++;
+    for (int i = 0; i < N; i++) R[i] = A[N-i-1];
 
-    int res[2];
-    for (int t = 0; t < 2; t++) {
-        fill(dp, dp+N, INF);
-        for (int i = 0; i < N; i++) {
-            *lower_bound(dp, dp+N, A[i]) = A[i];
-        }
-        res[t] = lower_bound(dp, dp+N, INF) - dp;
-        for (int i = 0; i < N/2; i++) swap(A[i], A[N-i-1]);
+    fill(dp, dp+N, INF);
+    for (int i = 0; i < N; i++) {
+        *upper_bound(dp, dp+N, R[i]) = R[i];
     }
-    printf("%d\n%d\n", res[1], res[0]);
+    printf("%ld\n", lower_bound(dp, dp+N, INF) - dp);
+
+    fill(dp, dp+N, INF);
+    for (int i = 0; i < N; i++) {
+        *lower_bound(dp, dp+N, A[i]) = A[i];
+    }
+    printf("%ld\n", lower_bound(dp, dp+N, INF) - dp);
 }
