cce994fc2a1ca625a9ddf4f036db1f35d730096b
aoibird <aoi@aoibird.com>
2020-08-03 17:47:11 +0800

    Submit Luogu 1091, 合唱队形, Luogu 1091 (WA)

--- luogu/1091_合唱队形.cpp
+++ luogu/1091_合唱队形.cpp
@@ -13,6 +13,8 @@ typedef pair<int,int> PII;
 const int MAXN = 100+10;
 int A[MAXN];
 int dp[MAXN][MAXN];
+int L[MAXN][MAXN];
+int R[MAXN][MAXN];
 int N;
 
 void print_2darray(int a[MAXN][MAXN], int r, int c)
@@ -22,20 +24,42 @@ void print_2darray(int a[MAXN][MAXN], int r, int c)
 }
 
 
+
+
 int main()
 {
     while (scanf("%d", &N) == 1) {
         for (int i = 0; i < N; i++) scanf("%d", &A[i]);
 
-        memset(dp, 0x3f, sizeof(dp));
-        for (int i = 0; i < N; i++) dp[i][i] = 0;
+        memset(dp, 0, sizeof(dp));
+        memset(L, 0, sizeof(L));
+        memset(R, 0, sizeof(R));
+        for (int i = 0; i < N; i++) { dp[i][i] = 1; L[i][i] = R[i][i] = i; }
         for (int i = N-1; i >= 0; i--) {
             for (int j = i+1; j <= N-1; j++) {
-                dp[i][j] = min(dp[i+1][j] + (A[i] >= A[i+1]),
-                               dp[i][j-1] + (A[j] >= A[j-1]));
+                int l, r;
+                l = L[i+1][j], r = R[i+1][j];
+                if (dp[i+1][j] + (A[i] < A[l]) > dp[i][j]) {
+                    dp[i][j] = dp[i+1][j] + (A[i] < A[l]);
+                    L[i][j] = (A[i] >= A[l]) ? l : i;
+                    R[i][j] = (A[j] >= A[r]) ? r : j;
+                    // printf("1 ");
+                }
+
+                l = L[i][j-1], r = R[i][j-1];
+                if (dp[i][j-1] + (A[j] < A[r]) > dp[i][j]) {
+                    dp[i][j] = dp[i][j-1] + (A[j] < A[r]);
+                    L[i][j] = (A[i] >= A[l]) ? l : i;
+                    R[i][j] = (A[j] >= A[r]) ? r : j;
+                    // printf("2 ");
+                }
+
+                // printf("(%d, %d): %d %d %d\n", i, j, dp[i][j], L[i][j], R[i][j]);
             }
         }
         // print_2darray(dp, N, N);
-        printf("%d\n", dp[0][N-1]);
+        // print_2darray(L, N, N);
+        // print_2darray(R, N, N);
+        printf("%d\n", N-dp[0][N-1]);
     }
 }
