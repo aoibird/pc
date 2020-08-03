aca93de288fe0ca98e5bd081405a9f182495168b
aoibird <aoi@aoibird.com>
2020-08-03 18:49:13 +0800

    Submit Luogu 1091, 合唱队形, Luogu 1091 (WA)

--- luogu/1091_合唱队形.cpp
+++ luogu/1091_合唱队形.cpp
@@ -10,56 +10,41 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
+const int INF = 1e9;
 const int MAXN = 100+10;
 int A[MAXN];
-int dp[MAXN][MAXN];
-int L[MAXN][MAXN];
-int R[MAXN][MAXN];
+int dp[MAXN];
 int N;
 
-void print_2darray(int a[MAXN][MAXN], int r, int c)
+void print_array(int *a, int n)
 {
-    for (int i = 0; i < r; i++)
-        for (int j = 0; j < c; j++) printf("%d%c", a[i][j], j==c-1?'\n':' ');
+    for (int i = 0; i < n; i++) printf("%d%c", a[i], i==n-1?'\n':' ');
 }
 
 
-
+bool cmp(const int &a, const int &b) { return a > b; }
 
 int main()
 {
-    while (scanf("%d", &N) == 1) {
+    while (scanf("%d", &N) == 1 && N) {
         for (int i = 0; i < N; i++) scanf("%d", &A[i]);
 
         memset(dp, 0, sizeof(dp));
-        memset(L, 0, sizeof(L));
-        memset(R, 0, sizeof(R));
-        for (int i = 0; i < N; i++) { dp[i][i] = 1; L[i][i] = R[i][i] = i; }
-        for (int i = N-1; i >= 0; i--) {
-            for (int j = i+1; j <= N-1; j++) {
-                int l, r;
-                l = L[i+1][j], r = R[i+1][j];
-                if (dp[i+1][j] + (A[i] < A[l]) > dp[i][j]) {
-                    dp[i][j] = dp[i+1][j] + (A[i] < A[l]);
-                    L[i][j] = (A[i] >= A[l]) ? l : i;
-                    R[i][j] = (A[j] >= A[r]) ? r : j;
-                    // printf("1 ");
-                }
-
-                l = L[i][j-1], r = R[i][j-1];
-                if (dp[i][j-1] + (A[j] < A[r]) > dp[i][j]) {
-                    dp[i][j] = dp[i][j-1] + (A[j] < A[r]);
-                    L[i][j] = (A[i] >= A[l]) ? l : i;
-                    R[i][j] = (A[j] >= A[r]) ? r : j;
-                    // printf("2 ");
-                }
-
-                // printf("(%d, %d): %d %d %d\n", i, j, dp[i][j], L[i][j], R[i][j]);
+        int m = 0;
+        for (int i = 1; i < N-1; i++) {
+            fill(dp, dp+i+1, INF);
+            for (int j = 0; j < i+1; j++) {
+                *lower_bound(dp, dp+i+1, A[j]) = A[j];
             }
+            int l1 = lower_bound(dp, dp+i+1, INF) - dp;
+            fill(dp+i, dp+N, 0);
+            for (int j = i; j < N; j++) {
+                *lower_bound(dp+i, dp+N, A[j], cmp) = A[j];
+            }
+            int l2 = lower_bound(dp+i, dp+N, 0, cmp) - (dp+i);
+            m = max(m, l1 + l2 - 1);
+            // printf("%d = %d + %d - 1(%d)\n", l1+l2-1, l1, l2, m);
         }
-        // print_2darray(dp, N, N);
-        // print_2darray(L, N, N);
-        // print_2darray(R, N, N);
-        printf("%d\n", N-dp[0][N-1]);
+        printf("%d\n", N-m);
     }
 }
