d355e0b0bec74e497254b159fae8ead29f3a693f
aoibird <aoi@aoibird.com>
2020-08-06 09:53:54 +0800

    Submit CPC Exercise 3-4, Cow Acrobats, POJ 3045 (WA)

---
+++ cpc/exercise3-4_cowacrobats.cpp
@@ -0,0 +1,40 @@
+// POJ 3045
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+
+const int INF = 1e9;
+const int MAXN = 50000+10;
+PII C[MAXN];
+int SW[MAXN];
+int N;
+
+bool cmp(const PII &a, const PII &b)
+{
+    return a.first + a.second < b.first + b.second;
+}
+
+int main()
+{
+    while (scanf("%d", &N) == 1) {
+        for (int i = 0; i < N; i++) scanf("%d%d", &C[i].first, &C[i].second);
+
+        sort(C, C+N, cmp);
+        SW[N-1] = 0;
+        for (int i = N-2; i >= 0; i--) {
+            SW[i] = SW[i+1] + C[i].first;
+        }
+        int m = -INF;
+        for (int i = 0; i < N; i++) {
+            m = max(m, SW[i] - C[i].second);
+        }
+        printf("%d\n", m);
+    }
+}
