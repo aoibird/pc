4e7d6b562cb5c6a5e346b8814b61c782081c433d
aoibird <aoi@aoibird.com>
2020-08-06 10:48:30 +0800

    Submit CPC Exercise 3-5, Dropping tests, POJ 2976 (AC)

--- cpc/exercise3-5_droppingtests.cpp
+++ cpc/exercise3-5_droppingtests.cpp
@@ -11,7 +11,6 @@ typedef long long ll;
 typedef pair<int,int> PII;
 
 const double ERR = 0.00001;
-const double INF = 1e9+10;
 const int MAXN = 1000+10;
 PII T[MAXN];
 double S[MAXN];
@@ -37,7 +36,7 @@ int main()
         for (int i = 0; i < N; i++) scanf("%d", &T[i].second);
         K = N - K;
 
-        double lb = 0, ub = INF;
+        double lb = 0, ub = 1000;
         while ((ub-lb) > ERR) {
             double mid = (lb+ub)/2;
             if (can(mid)) { lb = mid; }
