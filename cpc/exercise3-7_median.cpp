adfe6f9232536572b33feb45644cf849415263a1
aoibird <aoi@aoibird.com>
2020-08-08 20:13:15 +0800

    Submit CPC Exercise 3-7, Median, POJ 3579 (TLE)

---
+++ cpc/exercise3-7_median.cpp
@@ -0,0 +1,43 @@
+// POJ 3579
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> PII;
+
+const ll INF = (1LL << 40);
+const int MAXN = 100000+10;
+int A[MAXN];
+int N, M;
+
+bool can(ll x)
+{
+    ll cnt = 0;
+    for (int i = 0; i < N; i++) {
+        ll target = A[i] + x;
+        cnt += (A + N) - lower_bound(A, A+N, target);
+    }
+    return cnt <= M/2;
+}
+
+int main()
+{
+    while (scanf("%d", &N) == 1) {
+        for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+
+        sort(A, A+N);
+        M = N * (N - 1) / 2;
+        ll lb = 0, ub = INF;
+        while (ub - lb > 1) {
+            ll mid = lb+(ub-lb)/2;
+            if (can(mid)) ub = mid;
+            else lb = mid;
+        }
+        printf("%lld\n", lb);
+    }
+}
