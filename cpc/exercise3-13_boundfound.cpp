2983fca28efda6a78f05cf8eae58cae5c71a2a17
aoibird <aoi@aoibird.com>
2020-08-09 09:57:49 +0800

    Submit CPC Exercise 3-13, Bound Found, POJ 2566 (RE)

--- cpc/exercise3-13_boundfound.cpp
+++ cpc/exercise3-13_boundfound.cpp
@@ -12,18 +12,37 @@ typedef pair<int,int> PII;
 
 const int MAXN = 1e5+10;
 int A[MAXN];
+PII S[MAXN];
 int N, K;
+
+void print_array(PII *a, int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("(%d,%d)%c", a[i].first, a[i].second, i==n-1?'\n':' ');
+}
+
+void save_best(int q, int sum, int l, int r, int &s, int &f, int &t)
+{
+    if (abs(q-abs(sum)) < abs(q-abs(s))) {
+        f = min(S[l].second, S[r].second);
+        t = max(S[l].second, S[r].second);
+        s = sum;
+    }
+}
+
 
 void query(int q, int &s, int &f, int &t)
 {
-    int l = 0, r = 0, sum = 0;
-    while (l < N && r < N) {
-        while (r < N && abs(sum) < q) {
-            sum += A[r++];
-            if (abs(q-abs(sum)) < abs(q-s)) { f = l, t = r-1, s = abs(sum); }
+    int l = 0, r = 1; int sum = S[r].first - S[l].first;
+    while (true) {
+        while (r <= N && abs(sum) < q) {
+            r++; sum = S[r].first - S[l].first;
+            save_best(q, sum, l, r, s, f, t);
         }
+        if (abs(sum) < q) break;
 
-        sum -= A[l++];
+        l++; sum = S[r].first - S[l].first;
+        save_best(q, sum, l, r, s, f, t);
     }
 }
 
@@ -32,12 +51,16 @@ int main()
     while (scanf("%d%d", &N, &K) == 2) {
         if (N == 0 && K == 0) break;
         for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+        S[0] = PII(0, 0);
+        for (int i = 1; i <= N; i++) S[i] = PII(S[i-1].first + A[i-1], i);
+        sort(S, S+N+1);
+        // print_array(S, N+1);
 
         for (int i = 0; i < K; i++) {
             int q; scanf("%d", &q);
             int s = 0, f = 0, t = 0;
             query(q, s, f, t);
-            printf("%d %d %d\n", s, f+1, t+1);
+            printf("%d %d %d\n", abs(s), f+1, t);
         }
     }
 }
