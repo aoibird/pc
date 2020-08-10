1c0da34641cae5ac015868121240778b9d352edb
aoibird <aoi@aoibird.com>
2020-08-10 19:37:57 +0800

    Submit CPC Exercise 3-15, Graveyard Design, POJ 2100 (WA)

---
+++ cpc/exercise3-15_graveyarddesign.cpp
@@ -0,0 +1,38 @@
+// POJ 2100
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,ll> PLL;
+
+vector<PLL> V;
+ll N;
+
+int main()
+{
+    while (scanf("%lld", &N) == 1) {
+        ll l = 0, r = 0, sum = 0;
+        V.resize(0);
+        while (true) {
+            while (r*r < N && sum < N) { sum += r*r; r++; }
+            if (sum < N) break;
+
+            if (sum == N) V.push_back(PLL(l, r));
+            sum -= l*l; l++;
+        }
+        if (r * r == N) V.push_back(PLL(r, r+1));
+        printf("%ld\n", V.size());
+        for (int i = 0; i < V.size(); i++) {
+            ll l = V[i].first, r = V[i].second;
+            printf("%lld", r-l);
+            for (ll i = l; i < r; i++) printf(" %lld", i);
+            printf("\n");
+        }
+    }
+}
