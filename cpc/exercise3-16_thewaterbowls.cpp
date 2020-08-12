5c61c9de7a786ca19809106b90848f9978f9c9e7
aoibird <aoi@aoibird.com>
2020-08-12 11:23:30 +0800

    Submit CPC Exercise 3-16, The Water Bowls, POJ 3185 (WA)

--- cpc/exercise3-16_thewaterbowls.cpp
+++ cpc/exercise3-16_thewaterbowls.cpp
@@ -15,21 +15,36 @@ const int N = 20;
 int A[MAXN];
 int flip[MAXN];
 
+int flipit()
+{
+    int cnt = 0;
+    for (int i = 1; i < N; i++) {
+        if ((A[i-1] + flip[i-1])%2 == 1) {
+            flip[i-1]++; flip[i]++; flip[i+1]++;
+            cnt++;
+        }
+        else ;
+    }
+    return cnt;
+}
 
 int main()
 {
     while (scanf("%d", &A[0]) == 1) {
         for (int i = 1; i < N; i++) scanf("%d", &A[i]);
-        memset(flip, 0, sizeof(flip));
 
         int cnt = 0;
-        if (A[0] == 1) { flip[0] = flip[1] = 1; cnt++; }
-        for (int i = 1; i < N; i++) {
-            if ((A[i-1] + flip[i-1])%2 == 1) {
-                flip[i-1]++; flip[i]++; flip[i+1]++;
-                cnt++;
-            }
-            else ;
+        memset(flip, 0, sizeof(flip));
+        if (A[0] == 1) {
+            flip[0] = flip[1] = 1;
+            int cnt1 = flipit() + 1;
+            memset(flip, 0, sizeof(flip));
+            int cnt2 = flipit();
+
+            cnt = min(cnt1, cnt2);
+        }
+        else {
+            cnt = flipit();
         }
 
         printf("%d\n", cnt);
