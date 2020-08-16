719ceedb6107688d2c0660d30a46426b4c839c7f
aoibird <aoi@aoibird.com>
2020-08-16 12:47:35 +0800

    Submit CPC Exercise 3-24, Matrix, POJ 2155 (AC)

--- cpc/exercise3-24_matrix.cpp
+++ cpc/exercise3-24_matrix.cpp
@@ -7,14 +7,12 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-typedef long long ll;
+typedef int ll;
 typedef pair<int,int> PII;
 
 const int MAXN = 1000+5;
-const int MAXT = 50000+5;
 int A[MAXN][MAXN];
 int N, T;
-int I[MAXT], X1[MAXT], Y1[MAXT], X2[MAXT], Y2[MAXT];
 ll BIT[MAXN][MAXN];
 
 ll sum(ll bit[MAXN][MAXN], int x, int y)
@@ -31,41 +29,34 @@ void add(ll bit[MAXN][MAXN], int r, int c, int x, int y, int d)
         for (int j = y; j <= c; j+=j&-j) bit[i][j] += d;
 }
 
-void solve()
-{
-    for (int i = 0; i < T; i++) {
-        if (I[i] == 'C') {
-            // printf("%c: %d %d %d %d\n", I[i], X1[i], Y1[i], X2[i], Y2[i]);
-            add(BIT, N+1, N+1, X1[i], Y1[i], 1);
-            add(BIT, N+1, N+1, X1[i], Y2[i]+1, -1);
-            add(BIT, N+1, N+1, X2[i]+1, Y1[i], -1);
-            add(BIT, N+1, N+1, X2[i]+1, Y2[i]+1, 1);
-        }
-        else {
-            // printf("%c: %d %d\n", I[i], X1[i], Y1[i]);
-            ll t = sum(BIT, X1[i], Y1[i]);
-            printf("%lld\n", t%2);
-        }
-    }
-}
-
 int main()
 {
     int TC; scanf("%d", &TC);
     int ch;
     for (int i = 0; i < TC; i++) {
-        // printf("N == %d T == %d\n", N, T);
         scanf("%d%d", &N, &T);
         memset(BIT, 0, sizeof(BIT));
         while ((ch = getchar()) != EOF && ch != '\n') ;
+
         for (int i = 0; i < T; i++) {
-            I[i] = getchar();
-            if (I[i] == 'C') scanf("%d%d%d%d", &X1[i], &Y1[i], &X2[i], &Y2[i]);
-            else scanf("%d%d", &X1[i], &Y1[i]);
-             while ((ch = getchar()) != EOF && ch != '\n') ;
+            ch = getchar();
+            if (ch == 'C') {
+                int x1, y1, x2, y2;
+                scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
+                add(BIT, N+1, N+1, x1, y1, 1);
+                add(BIT, N+1, N+1, x1, y2+1, -1);
+                add(BIT, N+1, N+1, x2+1, y1, -1);
+                add(BIT, N+1, N+1, x2+1, y2+1, 1);
+            }
+            else {
+                int x, y;
+                scanf("%d%d", &x, &y);
+                ll t = sum(BIT, x, y);
+                printf("%d\n", t%2);
+            }
+            while ((ch = getchar()) != EOF && ch != '\n') ;
         }
 
-        solve();
         if (i+1 != TC) printf("\n");
     }
 }
