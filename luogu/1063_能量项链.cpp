a03ceaee1f9728e9d0a2d0a917b74321c4a4dfc7
aoibird <aoi@aoibird.com>
2020-08-19 17:46:16 +0800

    Submit Luogu 1063, 能量项链, Luogu 1063 (AC)

--- luogu/1063_能量项链.cpp
+++ luogu/1063_能量项链.cpp
@@ -10,35 +10,27 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
-const int MAXN = 100+5;
+const int MAXN = 1000+5;
 int A[MAXN];
 int N;
 int dp[MAXN][MAXN];
 
 int main()
 {
-    while (scanf("%d", &N) == 1 && N) {
-        for (int i = 0; i < N; i++) scanf("%d", &A[i]);
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) scanf("%d", &A[i]);
 
-        memset(dp, 0, sizeof(dp));
-        int maxv = 0;
-        for (int l = 2; l <= N; l++) {
-            for (int i = 0; i < N; i++) {
-                int j = (i+l-1) % N;
-                for (int t = 1; t <= l-1; t++) {
-                    int k = (i+t-1)%N, k1 = (i+t)%N;
-                    // printf("[%d %d %d %d] %d %d %d*%d*%d=%d\n", i, k, k1, j,
-                    //        dp[i][k], dp[k1][j], A[i], A[k1], A[(j+1)%N],
-                    //        A[i]*A[k1]*A[(j+1)%N]);
-                    dp[i][j] = max(dp[i][j], dp[i][k] + dp[k1][j] + A[i] * A[k1] * A[(j+1)%N]);
-                    maxv = max(maxv, dp[i][j]);
-                }
+    memset(dp, 0, sizeof(dp));
+    int maxv = 0;
+    for (int l = 2; l <= N; l++) {
+        for (int i = 0; i < N; i++) {
+            int j = (i+l-1)%N, j1 = (i+l)%N;
+            for (int t = 1; t < l; t++) {
+                int k = (i+t-1)%N, k1 = (i+t)%N;
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k1][j] + A[i] * A[k1] * A[j1]);
+                if (l == N) maxv = max(maxv, dp[i][j]);
             }
         }
-        // for (int i = 0; i < N; i++) {
-        //     for (int j = 0; j < N; j++) printf("%d ", dp[i][j]);
-        //     printf("\n");
-        // }
-        printf("%d\n", maxv);
     }
+    printf("%d\n", maxv);
 }
