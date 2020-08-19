67add68387250956b8851088f8d6d779ecee667a
aoibird <aoi@aoibird.com>
2020-08-19 22:14:20 +0800

    Submit Luogu 1005, 矩阵取数游戏, Luogu 1005 (AC)

--- luogu/1005_矩阵取数游戏.cpp
+++ luogu/1005_矩阵取数游戏.cpp
@@ -10,21 +10,55 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> PII;
 
-const int MAXN = 30+5;
-const int MAXM = 50+5;
+struct BigInt {
+    static const ll MOD = 1e18;
+    ll hi, lo;
+    BigInt(ll h=0, ll l=0) { hi=h; lo=l; }
+    BigInt(int a) { hi = 0; lo = a; }
+    BigInt operator+(const BigInt &a) const {
+        BigInt c(hi, lo);
+        c.lo += a.lo, c.hi += a.hi;
+        if (c.lo >= MOD) c.lo -= MOD, c.hi += 1;
+        return c;
+    }
+    BigInt operator*(const int &a) const {
+        BigInt c(hi, lo);
+        c.lo *= a, c.hi *= a;
+        if (c.lo >= MOD) c.lo -= MOD, c.hi += 1;
+        return c;
+    }
+    bool operator>(const BigInt &a) const {
+        return hi>a.hi || (hi==a.hi && lo > a.lo);
+    }
+    void print() {
+        if (hi) printf("%lld%018lld", hi, lo);
+        else printf("%lld", lo);
+    }
+};
+
+const int MAXN = 80+5;
+const int MAXM = 80+5;
 int matrix[MAXN][MAXM];
 int N, M;
-ll dp[MAXM][MAXM];
+BigInt dp[MAXM][MAXM];
 
-ll solve(int x)
+
+BigInt solve(int x)
 {
-    memset(dp, 0, sizeof(dp));
-    for (int i = 0; i < M; i++) dp[i][i] = matrix[x][i];
+    for (int i = 0; i < M; i++)
+        for (int j = 0; j < M; j++) dp[i][j] = BigInt(0);
+    for (int i = 0; i < M; i++) dp[i][i] = BigInt(matrix[x][i]);
     for (int i = M-2; i >= 0; i--) {
         for (int j = i+1; j < M; j++) {
-            dp[i][j] = max(matrix[x][i] + dp[i+1][j] * 2, matrix[x][j] + dp[i][j-1] * 2);
+            BigInt a = BigInt(matrix[x][i]) + dp[i+1][j]*2;
+            BigInt b = BigInt(matrix[x][j]) + dp[i][j-1]*2;
+            dp[i][j] = (a > b) ? a : b;
         }
     }
+    // for (int i = 0; i < M; i++) {
+    //     for (int j = 0; j < M; j++) { dp[i][j].print(); printf(" "); }
+    //     printf("\n");
+    // }
     return dp[0][M-1] * 2;
 }
 
@@ -35,7 +69,7 @@ int main()
         for (int j = 0; j < M; j++) scanf("%d", &matrix[i][j]);
     }
 
-    ll sum = 0;
-    for (int i = 0; i < N; i++) sum += solve(i);
-    printf("%lld\n", sum);
+    BigInt sum = 0;
+    for (int i = 0; i < N; i++) sum = sum + solve(i);
+    sum.print(); printf("\n");
 }
