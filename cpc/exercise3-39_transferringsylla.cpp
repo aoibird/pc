8c467d966e81d8a23873834e9498c920b7e38cc5
aoibird <aoi@aoibird.com>
2020-08-27 21:40:26 +0800

    Submit CPC Exercise 3-39, Transferring Sylla, POJ 3713 (TLE)

--- cpc/exercise3-39_transferringsylla.cpp
+++ cpc/exercise3-39_transferringsylla.cpp
@@ -21,9 +21,19 @@ const int MAXN = 500+5;
 const int INF = 100000;
 const int MAXM = 20000+5;
 PII E[MAXM];
-vector<Edge> G[MAXN*2];
-int level[MAXN*2], iter[MAXN*2];
+vector<Edge> G[MAXN];
+int level[MAXN], iter[MAXN];
+bool indep[MAXN];
 int N, M;
+
+void print_graph()
+{
+    for (int i = 0; i < N; i++) {
+        printf("%d: ", i);
+        for (int j = 0; j < G[i].size(); j++) printf(" (%d %d)", G[i][j].to, G[i][j].cap);
+        printf("\n");
+    }
+}
 
 void add_edge(int from, int to, int cap)
 {
@@ -33,15 +43,14 @@ void add_edge(int from, int to, int cap)
 
 void bfs(int s)
 {
-    memset(level, -1, sizeof(level));
-    queue<int> q;
+    memset(level, -1, sizeof(level)); queue<int> q;
     level[s] = 0; q.push(s);
     while (!q.empty()) {
         int v = q.front(); q.pop();
         for (int i = 0; i < G[v].size(); i++) {
             Edge &e = G[v][i];
-            if (e.cap > 0 && level[e.to] < 0) {
-                level[e.to] = level[v] + 1; q.push(e.to);
+            if (e.cap>0 && level[e.to]<0 && !indep[e.to]) {
+                level[e.to] = level[v]+1; q.push(e.to);
             }
         }
     }
@@ -52,9 +61,13 @@ int dfs(int v, int t, int f)
     if (v == t) return f;
     for (int &i = iter[v]; i < G[v].size(); i++) {
         Edge &e = G[v][i];
-        if (e.cap > 0 && level[v] < level[e.to]) {
+        if (e.cap > 0 && level[v] < level[e.to] && !indep[e.to]) {
             int d = dfs(e.to, t, min(f, e.cap));
-            if (d > 0) { e.cap -= d; G[e.to][e.rev].cap += d; return d; }
+            if (d > 0) {
+                e.cap -= d; G[e.to][e.rev].cap += d;
+                if (e.to != t) indep[e.to] = true;
+                return d;
+            }
         }
     }
     return 0;
@@ -63,65 +76,44 @@ int dfs(int v, int t, int f)
 int max_flow(int s, int t)
 {
     int flow = 0;
-    while (flow < 3) {
+    memset(indep, 0, sizeof(indep)); indep[s] = true;
+    while (true) {
         bfs(s);
         if (level[t] < 0) return flow;
         memset(iter, 0, sizeof(iter));
-        int f; while (flow < 3 && (f = dfs(s, t, INF)) > 0) flow += f;
+        int f; while ((f = dfs(s, t, INF)) > 0) flow += f;
     }
-    return flow;
 }
 
-void init_edge()
-{
-    for (int i = 0; i < N; i++) { add_edge(i, i+N, 1); }
-    for (int i = 0; i < M; i++) {
-        int a = E[i].first, b = E[i].second;
-        int a2 = a + N, b2 = b + N;
-        add_edge(b2, a, 1); add_edge(a2, b, 1);
-    }
-
-}
 
 void recover_edge()
 {
-    for (int i = 0; i < N*2; i++) {
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < G[i].size(); j++) G[i][j].cap = G[i][j].bak;
     }
 }
 
-void init_graph() { for (int i = 0; i < N*2; i++) G[i].clear(); }
-
-void print_graph()
-{
-    for (int i = 0; i < N*2; i++) {
-        printf("%d: ", i);
-        for (int j = 0; j < G[i].size(); j++) printf(" (%d %d)", G[i][j].to, G[i][j].cap);
-        printf("\n");
-    }
-}
 
 int main()
 {
     while (scanf("%d%d", &N, &M) == 2) {
         if (N == 0 && M == 0) break;
         // printf("N, M = %d %d\n", N, M);
+        for (int i = 0; i < N; i++) G[i].clear();
         for (int i = 0; i < M; i++) scanf("%d%d", &E[i].first, &E[i].second);
 
-        // init_graph(), init_edge();
-        // print_graph();
-
-        init_graph(); init_edge();
+        for (int i = 0; i < M; i++) {
+            add_edge(E[i].first, E[i].second, 1);
+            add_edge(E[i].second, E[i].first, 1);
+        }
         bool yes = true;
-        for (int i = 0; i < N; i++) {
-            for (int j = i+1; j < N; j++) {
+        for (int i = 0; i < N && yes; i++) {
+            for (int j = i+1; j < N && yes; j++) {
                 if (i == j) continue;
                 recover_edge();
-                int flow = max_flow(i+N, j);
-                // printf("%d-%d %d\n", i, j, flow);
-                if (flow < 3) { yes = false; break; }
+                int flow = max_flow(i, j);
+                if (flow < 3) yes = false;
             }
-            if (yes == false) break;
         }
 
         if (yes) printf("YES\n");
