179cd1fe5896dfd5e75a68548e187d13ba9e81f2
aoibird <aoi@aoibird.com>
2021-07-01 00:40:26 +0800

    Submit LOJ 162, 快速幂 2, LOJ 162 (WA)

--- loj/162_快速幂2.cpp
+++ loj/162_快速幂2.cpp
@@ -11,12 +11,10 @@
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> PII;
-typedef pair<int, ll> PIL;
 
 const int P = 998244352;
 const int MAXN = 5000000 + 10;
-PII A[MAXN];
-PII R[MAXN];
+int A[MAXN];
 
 // x^n % m
 ll power(ll x, ll n, ll m) {
@@ -35,25 +33,15 @@ int main() {
 
     while (scanf("%d%d", &X, &N) == 2) {
         for (int i = 0; i < N; i++) {
-            int a;
-            scanf("%d", &a);
-            A[i] = PII(a, i);
+            scanf("%d", &A[i]);
         }
-
-        sort(A, A + N);
 
         ll f = 1;
 
         for (int i = 0; i < N; i++) {
-            int d = (i == 0) ? A[i].first : A[i].first - A[i - 1].first;
-            f = (d > 0) ? f * power(X, d, P) : f;
-            R[i] = PII(A[i].second, f);
-        }
-
-        sort(R, R + N);
-
-        for (int i = 0; i < N; i++) {
-            printf("%d%c", R[i].second, (i + 1 == N) ? '\n' : ' ');
+            int d = (i == 0) ? A[i] : A[i] - A[i - 1];
+            f = (d > 0) ? f * power(X, d, P) : (d < 0) ? f / power(X, -d, P) : f;
+            printf("%lld%c", f, (i + 1) == N ? '\n' : ' ');
         }
     }
 }
